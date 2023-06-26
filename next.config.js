c7cad515027982246f904d9b2dc0502f7add7cda
MomboteQ <momboteq@gmail.com>
2023-06-26 16:13:04 +0200

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: 'export',
-    //basePath: '/<repository-name`>',
+    output: 'export',
+    basePath: '/<repository-name`>',
 };
 
 module.exports = nextConfig;
